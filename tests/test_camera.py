0d055bebc4002d3ed4505d3cd94636fc0ef129db
Dave Nilosek <david.nilosek@eagleview.com>
2020-01-09 08:38:46 -0800

    Set up test

---
+++ tests/test_camera.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Tests for camera class."""
+
+import unittest
+
+from evtech import Camera
+
+class TestCamera(unittest.TestCase):
+    """Tests for `evtech` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
