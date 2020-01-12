7589f2bdbef5091d3a45df40ab83190afdd47b61
Dave Nilosek <david.nilosek@eagleview.com>
2020-01-12 14:01:42 -0800

    Don't need test data in repo:

---
+++ tests/test_dataset.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Tests for dataset functions"""
+
+import unittest
+
+
+class TestDataset(unittest.TestCase):
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        pass
+
+    
